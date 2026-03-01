a06988c4e9882f16d3bc176b758c1bb10beadf5b
Harika Baddukonda <147475012+Harika-75@users.noreply.github.com>
2026-03-01 23:09:41 +0530

    Add files via upload

--- Hire A driver Tables.sql
+++ Hire A driver Tables.sql
@@ -30,4 +30,21 @@ FOREIGN KEY (id) REFERENCES users(id)
 
 SELECT*FROM bookings;
 
+INSERT INTO bookings (id, source, destination, booking_date, booking_time, status)
+VALUES
+(1, 'Visakhapatnam', 'Hyderabad', '2026-03-10', '10:30:00', 'PENDING');
 
+INSERT INTO bookings (id, source, destination, booking_date, booking_time, status)
+VALUES
+(1, 'Visakhapatnam', 'Hyderabad', '2026-03-10', '10:30:00', 'CONFIRMED');
+ 
+INSERT INTO bookings (id, source, destination, booking_date, booking_time, status)
+VALUES
+(2, 'Visakhapatnam', 'Hyderabad',  '2026-03-03', '09:00:00', 'PENDING'),
+(2, 'Visakhapatnam', 'Bangalore',  '2026-03-11', '10:30:00', 'CONFIRMED'),
+(3, 'Chennai',       'Hyderabad',  '2026-03-01', '11:15:00', 'PENDING'),
+(3, 'Chennai',       'Bangalore',  '2026-02-13', '12:00:00', 'COMPLETED'),
+(4, 'Hyderabad',     'Pune',       '2026-02-14', '08:45:00', 'PENDING'),
+(4, 'Hyderabad',     'Mumbai',     '2026-03-15', '14:30:00', 'CONFIRMED');
+-- (5, 'Bangalore',     'Chennai',    '2026-03-16', '16:00:00', 'CANCELLED'), (5, 'Bangalore',     'Visakhapatnam','2026-03-17','18:20:00', 'PENDING'),(6, 'Pune',          'Mumbai',     '2026-02-18', '07:30:00', 'COMPLETED'),
+--(6,'Mumbai',        'Delhi',      '2026-03-19', '21:00:00', 'CONFIRMED');
